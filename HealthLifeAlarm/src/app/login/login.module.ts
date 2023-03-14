efb3b2b8aada63963dbf311c5d4747c1495c16f9
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2023-03-13 23:19:48 -0500

    Ajuste paginas crear cuenta y login

--- HealthLifeAlarm/src/app/login/login.module.ts
+++ HealthLifeAlarm/src/app/login/login.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { AppRoutingModule } from '../app-routing.module';
+import {MatInputModule} from '@angular/material/input';
+
+const uiModules = [
+  MatInputModule
+];
+
 
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    uiModules
   ],
   declarations: [LoginComponent],
-  exports: [LoginComponent]
+  exports: [LoginComponent, uiModules]
 })
 export class LoginModule { }
